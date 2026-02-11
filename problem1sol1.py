b47561d5d8d505953cc6a24c4ddce162b52a6d70
KianVD <159494611+KianVD@users.noreply.github.com>
2026-02-11 14:00:05 -0800

    Add files via upload

---
+++ problem1sol1.py
@@ -0,0 +1,21 @@
+import math
+#given nums, find contiguous subarray with largest sum
+#brute force
+def largestsubarray(nums):
+    numslen = len(nums)
+    largestsum = -math.inf
+    for i in range(numslen):#one loop for each starting index
+        for j in range(numslen):#one loop for each subset length
+            subset = nums[i:i+j+1] #TODO might be out of range
+            subsetsum = sum(subset)
+            if subsetsum > largestsum:
+                largestsum = subsetsum
+
+    return largestsum
+
+
+print(largestsubarray([-2,1,-3,4,-1,2,1,-5,4]))
+
+print(largestsubarray([1]))
+
+print(largestsubarray([5,4,-1,7,8]))
---
+++ problem1sol2.py
@@ -0,0 +1,20 @@
+import math
+#given nums, find contiguous subarray with largest sum
+#efficient
+def largestsubarray(nums):
+    numslen = len(nums)
+    largestsum = -math.inf
+    #for every positive number
+    pointer = nums[0]
+    
+
+
+
+    return largestsum
+
+
+print(largestsubarray([-2,1,-3,4,-1,2,1,-5,4]))
+
+print(largestsubarray([1]))
+
+print(largestsubarray([5,4,-1,7,8]))
